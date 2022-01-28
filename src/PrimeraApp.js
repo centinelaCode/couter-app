c8b26aa851d11265013043b567968a64649085e6
centinelacode <centinela1024tb@gmail.com>
2022-01-28 17:57:51 -0600

    Primeros pasos en React

---
+++ src/PrimeraApp.js
@@ -0,0 +1,13 @@
+
+// Funcional Components
+const PrimeraApp = () => {
+   return (
+      <>
+         <h1>Hola Mundo</h1>      
+         <p>Mi primera App</p>
+      </>
+   );
+        
+}
+
+export default PrimeraApp;
